e34eb9b5acea6f386e595d627c27ddcda8ed682e
nisslyak <nissopodolyak@gmail.com>
2024-05-31 12:29:53 +0300

    First commit

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System; 
+
+
+
+class Result
+{
+
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int findMedian(List<int> arr)
+    {
+        arr.Sort();
+        int median;
+        if (arr.Count % 2 == 0) {
+            median = arr[arr.Count / 2 - 1] + arr[arr.Count /2];
+        }
+        else 
+        {
+            median = arr[arr.Count / 2];
+        }
+        return median;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.findMedian(arr);
+    }
+}
